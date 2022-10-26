756b8423959f345d02f89ce3389de3f4e125a9f9
Levi-Pongthakorn <116298615+Levi-Pongthakorn@users.noreply.github.com>
2022-10-27 01:02:41 +0700

    L3

--- app/src/main/java/com/example/cucanteen/HomeUser.java
+++ app/src/main/java/com/example/cucanteen/HomeUser.java
@@ -2,7 +2,9 @@ package com.example.cucanteen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeUser extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class HomeUser extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_user);
+    }
+    public void openstore(View v)
+    {
+        Intent i = new Intent(this, order2.class);
+        startActivity(i);
     }
 }
